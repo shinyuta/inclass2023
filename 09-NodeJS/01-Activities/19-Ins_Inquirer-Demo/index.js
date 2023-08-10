c6066dd4b7843cea28e658bd70179e4e6fc23a1c
Yuta Kirihara <yutakirihara@Stan-LOONAbook-m1.local>
2023-08-09 21:23:28 -0500

    W9D3 NodeD3 3rd party packages, for of, rest, spread, obj destructuring, array destructering

--- 09-NodeJS/01-Activities/19-Ins_Inquirer-Demo/index.js
+++ 09-NodeJS/01-Activities/19-Ins_Inquirer-Demo/index.js
@@ -1,25 +1,39 @@
 const inquirer = require('inquirer');
+const fs = require('fs')
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your user name?',
+      message: 'What is your name?',
       name: 'username',
     },
     {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
+      type: 'list',
+      message: 'What languages do you know?',
+      name: 'languages',
+      choices: ['English', 'Spanish', 'Mandarin', ]
     },
     {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
+      type: 'list',
+      // checkbox doesn't write to txt file for some reason.
+      message: 'what is your preferred method of communication?',
+      name: 'communication',
+      choices: ["texting", 'verbal', 'discord', 'email']
     },
   ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+
+  // .then((response) =>
+  //   response.confirm === response.password
+  //     ? console.log('Success!')
+  //     : console.log('You forgot your password already?!')
+  // );
+
+  .then(function (response) {
+    let data = response.username + " " + response.langauges + " " + response.communication;
+    fs.writeFile('Output.txt', data, (err) =>  {
+      if (err) throw err;
+    })
+  });
+ 
+// check solved for json file.
